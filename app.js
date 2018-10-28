69aa1739a6ef191e64f9246c86b02ce24dad23df
Tyler Mustard <tjm.0084@gmail.com>
2018-10-28 15:23:13 -0600

    init

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 var landingBackground = document.querySelector("#landing-bg");
-// var blueSquare = document.querySelector("#blueSquare");
-// var greenPentagon = document.querySelector("#greenPentagon");
+var navBar = document.querySelector(".topnav");
+// var stickyDiv = "sticky";
+// var landingTop = document.querySelector(".landing-wrapper");
+var fixed = false;
+var fixPoint = getDistance();
+// var readout = document.getElementById("readout");
+// var win = document.defaultView;
+var yScrollPosition;
+
+function getDistance() {
+  var topDist = navBar.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(e) {
+  var distance = getDistance() - window.pageYOffset;
+  var offset = window.pageYOffset;
+  // readout.innerHTML = fixPoint + "   " + distance + "   " + offset + "   " + fixed;
+  if ( (distance <= 0) && !fixed) {
+    navBar.style.position = "fixed";
+    navBar.style.top = "0px";
+    fixed = true;
+  } else if (fixed && (offset <= fixPoint)){
+    navBar.style.position = "relative";
+    navBar.style.top = "-25vh";
+    fixed = false;
+    
+  }
+};
+
+
 
 function setTranslate(xPos, yPos, el) {
   el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
 }
 
 window.addEventListener("DOMContentLoaded", scrollLoop, false);
- 
-var xScrollPosition;
-var yScrollPosition;
- 
+
 function scrollLoop() {
-  xScrollPosition = window.scrollX;
   yScrollPosition = window.scrollY;
   setTranslate(0, yScrollPosition * -0.2, landingBackground);
-  // setTranslate(0, yScrollPosition * -1.5, blueSquare);
-  // setTranslate(0, yScrollPosition * .5, greenPentagon);
   requestAnimationFrame(scrollLoop);
 }
+
+// window.onscroll = function () {
+//   console.log("1");
+//   if (win.scrollTop() > landingTop.height()) {
+//     console.log("hit");
+//     yourNavigation.addClass(stickyDiv);
+//   } else {
+//     yourNavigation.removeClass(stickyDiv);
+//     console.log("yo");
+//   }
+// };
